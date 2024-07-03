63c45d9b3bfda247956533f89cae2888d3fdae80
nimod7890 <nimod1234@g.skku.edu>
2024-07-03 14:35:41 +0900

    feat: 구독해지 dialog css load in html file

--- src/style/loadStyles.js
+++ src/style/loadStyles.js
@@ -8,12 +8,16 @@ const stylesheets = [
   "src/components/overlays/overlay.css",
   "src/components/overlays/dialog/dialog.css",
   "src/components/overlays/toast/toast.css",
+
+  "src/features/unsubscribeDialog/unsubscribeDialog.css",
 ];
+
 stylesheets.forEach(loadCSS);
 
 function loadCSS(url) {
   const link = document.createElement("link");
   link.rel = "stylesheet";
+  link.type = "text/css";
   link.href = url;
   document.head.appendChild(link);
 }
