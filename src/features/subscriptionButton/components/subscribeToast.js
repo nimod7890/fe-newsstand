ce5e3bdd5178ffbe86181996463e6e6c9aba8290
nimod7890 <nimod1234@g.skku.edu>
2024-07-09 15:06:02 +0900

    refactor: 구독 시 노출되는 toast 파일 분리

---
+++ src/features/subscriptionButton/components/subscribeToast.js
@@ -0,0 +1,19 @@
+import { showToast } from "../../../components/overlays/toast/toast.js";
+import { switchCompanyTab } from "../../renderNews/utils/updateStates.js";
+import { dispatchStorageEvent } from "../utils/dispatchStorageEvent.js";
+import { addSubscribedCompany } from "../utils/localStorage.js";
+
+const TOAST_SHOWING_TIME = 2000;
+
+/**
+ * @param {Company} company
+ */
+export function showSubscribeToast(company) {
+  showToast("내가 구독한 언론사에 추가되었습니다.", TOAST_SHOWING_TIME);
+  addSubscribedCompany(company);
+  setTimeout(() => {
+    switchCompanyTab("subscribed-news-tab");
+  }, TOAST_SHOWING_TIME);
+
+  dispatchStorageEvent({ company, isSubscribed: true });
+}
