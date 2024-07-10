a0cdc2936c8e619f3dbf7ac2a88ead5ad60d6970
nimod7890 <nimod1234@g.skku.edu>
2024-07-10 16:42:45 +0900

    rename: headline news url props

--- src/data/headlineNews.js
+++ src/data/headlineNews.js
@@ -1,15 +1,15 @@
 export const leftNewsItems = [
-  { id: 1, title: "[1보] 김기현·안철수·천하람·황교안, 與전대 본경선 진출", link: "#" },
-  { id: 2, title: "뉴스 항목 2", link: "#" },
-  { id: 3, title: "뉴스 항목 3", link: "#" },
-  { id: 4, title: "뉴스 항목 4", link: "#" },
-  { id: 5, title: "뉴스 항목 5", link: "#" },
+  { id: 1, title: "[1보] 김기현·안철수·천하람·황교안, 與전대 본경선 진출", url: "#" },
+  { id: 2, title: "뉴스 항목 2", url: "#" },
+  { id: 3, title: "뉴스 항목 3", url: "#" },
+  { id: 4, title: "뉴스 항목 4", url: "#" },
+  { id: 5, title: "뉴스 항목 5", url: "#" },
 ];
 
 export const rightNewsItems = [
-  { id: 1, title: "뉴스 항목 1", link: "#" },
-  { id: 2, title: "뉴스 항목 2", link: "#" },
-  { id: 3, title: "뉴스 항목 3", link: "#" },
-  { id: 4, title: "뉴스 항목 4", link: "#" },
-  { id: 5, title: "뉴스 항목 5", link: "#" },
+  { id: 1, title: "뉴스 항목 1", url: "#" },
+  { id: 2, title: "뉴스 항목 2", url: "#" },
+  { id: 3, title: "뉴스 항목 3", url: "#" },
+  { id: 4, title: "뉴스 항목 4", url: "#" },
+  { id: 5, title: "뉴스 항목 5", url: "#" },
 ];
