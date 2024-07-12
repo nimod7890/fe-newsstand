d74308bb3ca945d023502f4c1fff2fd9df180bc5
nimod7890 <nimod1234@g.skku.edu>
2024-07-12 11:38:08 +0900

    rename: .company-nav-button -> .adjacent-button

--- src/features/renderNews/components/@common/mainArrowButton/mainArrowButton.js
+++ src/features/renderNews/components/@common/mainArrowButton/mainArrowButton.js
@@ -12,8 +12,8 @@ import { createIcon } from "../../../../../components/icon/icon.js";
  */
 export function createMainArrowButton({ direction, onClick }) {
   const button = document.createElement("button");
-  button.className = `company-nav-button ${direction}`;
-  button.setAttribute("aria-label", direction === "prev" ? "이전 언론사" : "다음 언론사");
+  button.className = `adjacent-button ${direction}`;
+  button.setAttribute("aria-label", direction === "prev" ? "이전" : "다음");
 
   const icon = createIcon({ iconId: "arrow" });
   button.appendChild(icon);
